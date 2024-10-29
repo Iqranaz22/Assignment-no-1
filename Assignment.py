55eacb3bdd486951ab4b343e8f2279b62f6fd406
Iqranaz22 <iqranaz485@gmail.com>
2024-10-29 11:09:38 -0700

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,15 @@
+#sum=a+b+c
+a=20
+b=30
+c=40
+print(a+b+c)
+
+diffrernce=a-b-c
+print(a-b-c)
+
+product=a*b*c
+print(a*b*c)
+
+average=a+b+c/3
+print(a+b+c/3)
+
---
+++ Question2.py
@@ -0,0 +1,25 @@
+#area=0.5*b*h
+b=10
+h=15
+area=0.5*10*15
+print("area of triangle",0.5*10*15)
+
+
+#area=s*s
+s=24
+area=s*s
+print("area of square",24*24)
+
+
+
+#area=3.14*r*r
+r=10
+area=3.14*r*r
+print("area of circle",3.14*10*10)
+
+
+#area=l*W
+l=20
+W=10
+area=l*W
+print("area of rectangle",20*10)
